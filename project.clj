fcf12567792e05f6b1cb8b2aac251d1583d85b3d
Nicolas Simonnet <ns@epithelix.com>
2019-10-15 16:30:08 +0200

    renamed stuff in project.clj

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject backup-desabuse "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject simple-backup "0.1.0-SNAPSHOT"
+  :description "A simple backup program running rsync in background"
+  :url "https://github.com/toxnico/simple-backup"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/math.combinatorics "0.1.6"]]
-  :repl-options {:init-ns backup-desabuse.core}
-  :main backup-desabuse.core
+  :repl-options {:init-ns simple-backup.core}
+  :main simple-backup.core
   :aot :all)
